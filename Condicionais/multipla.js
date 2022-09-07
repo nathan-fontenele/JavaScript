cc420aaab5146ff078ffe626dead93b31fc0fbef
Nathan Fontenele <nathangf60@outlook.com>
2022-09-07 17:39:23 -0300

    Condições multiplas

---
+++ Condicionais/multipla.js
@@ -0,0 +1,15 @@
+var number = 10;
+
+switch(number){
+    case 1:
+        console.log("Number is 1");
+        break;
+    case 2:
+        console.log("Number is 2");
+        break;
+    case 3:
+        console.log("Number is 3");
+        break;
+    default:
+        console.log("Number is not 1, 2 or 3");
+}
