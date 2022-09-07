908a6e9fa0b6723b8e14dea89cf5a76673570554
Nathan Fontenele <nathangf60@outlook.com>
2022-09-07 14:17:54 -0300

    Introdução ao DOM

---
+++ IntroducaoAoDOM/main.js
@@ -0,0 +1,29 @@
+//Select Tag
+var selectTagName = document.getElementsByTagName("p")[0];
+
+selectTagName.style.background = '#13747d';
+
+//Select ID
+var selectId = document.getElementById('slctId');
+
+selectId.style.background = '#fc354c';
+
+//Select Class
+var selectClass = document.getElementsByClassName('slctClass')[0];
+
+selectClass.style.background = '#0abfbc';
+
+//Query Selector ID
+var selectQueryId = document.querySelector('p#slctIdQuery');
+
+selectQueryId.style.background = '#29221f';
+selectQueryId.style.color = '#fff';
+
+//Query Selector Class
+var selectQueryClass = document.querySelector('p.slctClassQuery');
+
+selectQueryClass.style.background = '#fcf7c5';
+
+//Select Name
+var selectName = document.getElementsByName('slctName')[0];
+selectName.style.background = '#f7f1e3';
