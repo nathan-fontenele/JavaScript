9596b069814dafc26b01c0a42f9401eed6dcc734
Nathan Fontenele <nathangf60@outlook.com>
2022-09-15 16:18:18 -0300

    Alteração de lógica

--- Condicionais/MedidorDeVelocidade/main.js
+++ Condicionais/MedidorDeVelocidade/main.js
@@ -12,7 +12,7 @@ function verificar() {
 
         result.innerHTML += `<br>Você foi <strong>multado</strong> por excesso de velocidade.`;
     } else{
-        result.innerHTML = `Sua velocidade atual é de ${capturaVelocidade.value}Km/h`};
-
+        result.innerHTML = `Sua velocidade atual é de ${capturaVelocidade.value}Km/h`
         result.innerHTML += `<br>Você está dentro do limite de velocidade. <strong>Dirija com segurança!</strong>`;
+    };    
 }
